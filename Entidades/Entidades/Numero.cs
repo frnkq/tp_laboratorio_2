532adacf10d51953b33263f73c4d926997dc97a0
Franco Canevali <francocanevali@gmail.com>
2018-08-29 21:57:16 -0300

    tp1 0.0

---
+++ Entidades/Entidades/Numero.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+        private string SetNumero
+        {
+            set
+            {
+                double n = ValidarNumero(value);
+                numero = n;
+            }
+        }
+
+        public Numero() { }
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string numero)
+        {
+            //TODO: validacion?
+            this.numero = ValidarNumero(numero);
+        }
+
+        /// <summary>
+        /// Valida que el valor recibido sea numerico y lo retorna en formato double, de no ser numerico, retorna 0
+        /// </summary>
+        /// <param name="strNumero"></param>
+        /// <returns>El valor del parametro en formato double si este es numerico, 0 si no lo es</returns>
+        private double ValidarNumero(string strNumero)
+        {
+            double number;
+            if (double.TryParse(strNumero, out number))
+                return number;
+            return 0;
+        }
+
+        /// <summary>
+        /// Convierte un numero binario representado en un string, a un numero decimal
+        /// </summary>
+        /// <param name="binario">String que representa el numero binario</param>
+        /// <returns>Devuelve el numero decimal que representa el binario, en formato string</returns>
+        public string BinarioDecimal(string binario)
+        {
+            bool converted = false;
+            string result = "";
+
+            if (converted)
+                return result;
+            return "Valor invalido";
+        }
+
+        /// <summary>
+        /// Convierte un numero decimal a un numero binario
+        /// </summary>
+        /// <param name="numero">Numero decimal a ser convertido en binario</param>
+        /// <returns>Devuelve el numero binario que representa al decimal, en formato string</returns>
+        public string DecimalBinario(double numero)
+        {
+            return "";
+        }
+
+
+        /**Operators**/
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator *(Numero n1, Numero n2)
+        {
+           return n1.numero* n2.numero;
+        }
+        public static double operator /(Numero n1, Numero n2)
+        {
+            if (n2.numero == 0)
+                return -1; //TODO: change this value
+            return n1.numero / n2.numero;
+        }
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        /**\Operators**/
+        
+
+    }
+}
---
+++ Entidades/Entidades/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero n1 = new Numero("82");
+            Numero n2 = new Numero(4);
+            Numero zero = new Numero(0);
+
+            double suma = n1 + n2;
+            double division = n1 / n2;
+            double failureDivision = n1 / zero;
+
+            Console.WriteLine("Suma {0} division {1} failureDivision {2}", suma, division, failureDivision);
+
+
+            Console.ReadKey();
+        }
+    }
+}
